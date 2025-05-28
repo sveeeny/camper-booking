25617d7b9efccf4c52624ec9c34f09147079b8ab
Sveeeny <svenherger@bluewin.ch>
2025-05-28 10:06:31 +0200

    backend fix stripe.module

--- backend/src/stripe/stripe.module.ts
+++ backend/src/stripe/stripe.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { StripeService } from './stripe.service';
 import { StripeController } from './stripe.controller';
 import { ConfigModule } from '@nestjs/config'; 
+import { BookingModule } from '@/booking/booking.module';
 
 @Module({
-  imports: [ConfigModule], 
+  imports: [
+    ConfigModule, 
+    BookingModule
+  ], 
   controllers: [StripeController],
   providers: [StripeService],
   exports: [StripeService], 
